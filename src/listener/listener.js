2482b5290032248e3d7b48a3bd88b4a97053ce29
galalkoro98 <galal.koro@gmail.com>
2023-01-21 17:22:14 +0100

    rename file

---
+++ src/listener/listener.js
@@ -0,0 +1,21 @@
+import { handler } from "../handler/handler.js";
+import { inputAdd, inputUpdate, tasksForm } from "../myVars.js";
+import { updateTask } from "../logic/updateTask.js";
+import { addTask } from "../logic/addTask.js";
+
+export const listener = () => {
+  handler();
+  inputAdd.addEventListener("click", addTask);
+  inputUpdate.addEventListener("click", updateTask);
+  tasksForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (e.target.classList.contains("input-add")) {
+      addTask();
+    }
+
+    if (e.target.classList.contains("input-update")) {
+      updateTask();
+    }
+  });
+};
